94d0ceeb09dcade17dfc65e4ee4e6a9b19d06f13
Rajkamal Rajendran <rajkamal8825@gmail.com>
2024-04-25 19:46:52 -0400

    lets see if it fixed it

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@ terraform {
     bucket = "insidertradingstatefile"
     key    = "firstproject/one"
     region = "us-east-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
   }
 }
 
--- terraform/var.tf
+++ terraform/var.tf
@@ -1,2 +1,2 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
